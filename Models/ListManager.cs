d63c107fe556f17aafde94f46b1c91b1411b5f75
okancaliskan <okancaliskann7@gmail.com>
2025-04-26 13:20:20 +0300

    Okan: ListManager.cs dosyası eklendi

--- Models/ListManager.cs
+++ Models/ListManager.cs
@@ -1,6 +1,71 @@
-﻿namespace Veri_Yapıları_Proje.Models
+﻿using System;
+
+namespace Veri_Yapilari_Proje.Models
 {
     public class ListManager
     {
+        // Farklı liste türleri için nesneler
+        private SinglyLinkedList singlyList = new SinglyLinkedList();
+        private DoublyLinkedList doublyList = new DoublyLinkedList();
+        private CircularLinkedList circularList = new CircularLinkedList();
+
+        // Kullanıcıdan gelen işlem komutunu çalıştırır
+        public void ProcessCommand(string listType, string action, int value)
+        {
+            switch (listType)
+            {
+                case "singly":
+                    HandleAction(singlyList, action, value);
+                    break;
+                case "doubly":
+                    HandleAction(doublyList, action, value);
+                    break;
+                case "circular":
+                    HandleAction(circularList, action, value);
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz liste türü.");
+                    break;
+            }
+        }
+
+        // İşlem türüne göre ekle, sil, ara, gez fonksiyonlarını çalıştırır
+        private void HandleAction(dynamic list, string action, int value)
+        {
+            switch (action)
+            {
+                case "add":
+                    list.Add(value);
+                    break;
+                case "remove":
+                    list.Remove(value);
+                    break;
+                case "search":
+                    list.Search(value);
+                    break;
+                case "traverse":
+                    list.Traverse();
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz işlem.");
+                    break;
+            }
+        }
+
+        // Listeyi JSON formatına çevirip döndürür
+        public string GetListAsJson(string listType)
+        {
+            switch (listType)
+            {
+                case "singly":
+                    return ListSerializer.SerializeSinglyList(singlyList);
+                case "doubly":
+                    return ListSerializer.SerializeDoublyList(doublyList);
+                case "circular":
+                    return ListSerializer.SerializeCircularList(circularList);
+                default:
+                    return "{}"; // Hatalı tür gelirse boş JSON döndür
+            }
+        }
     }
 }
