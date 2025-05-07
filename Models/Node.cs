3288abce11f601534dc42ed2c66aee8a0f8631c1
okancaliskan <okancaliskann7@gmail.com>
2025-05-07 14:02:58 +0300

    Add Node.cs file

--- Models/Node.cs
+++ Models/Node.cs
@@ -1,6 +1,16 @@
-﻿namespace Veri_Yapıları_Proje.Models
+﻿namespace Veri_Yapilari_Proje.Models
 {
     public class Node
     {
+        public int Value { get; set; }       // Düğümün taşıdığı değer
+        public Node Next { get; set; }       // Sonraki düğüme bağlantı (tek, çift, döngüsel için)
+        public Node Prev { get; set; }       // Önceki düğüme bağlantı (çift yönlü için)
+
+        public Node(int value)
+        {
+            Value = value;
+            Next = null;
+            Prev = null;
+        }
     }
 }
